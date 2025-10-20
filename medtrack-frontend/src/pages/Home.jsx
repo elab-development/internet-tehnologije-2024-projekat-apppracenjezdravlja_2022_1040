6e11944713964b4ee37a190d7d7435a8f5e578c2
mladen1811 <gajicmladengm@gmail.com>
2025-10-20 14:57:58 +0200

    Početni deo

---
+++ medtrack-frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>MedTrack</h1>
+      <p>Aplikacija za digitalnu evidenciju zdravstvenih kartona i praćenje zdravlja.</p>
+      
+    </div>
+  );
+}
